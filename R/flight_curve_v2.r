8d5ab992cac028b8f75cc31dc2f45f4c7a9a1c8a
RetoSchmucki <reto.schmucki@mail.mcgill.ca>
2016-07-04 17:39:33 +0100

    fix line error in the script

--- R/flight_curve_v2.r
+++ R/flight_curve_v2.r
@@ -109,7 +109,8 @@ flight_curve.v2 <- function(your_dataset) {
                 sp_data_all[sp_data_all$trimDAYNO > 305, "FITTED"] <- 0
                 # if infinite number in predict replace with NA.
                 if(sum(is.infinite(sp_data_all[, "FITTED"]))>0){
-                    print(paste("Error in the flight period for",sp_data_all$SPECIES[1],"at year", y, " weird predicted values"))                    sp_data_all[, "FITTED"] <- NA
+                    print(paste("Error in the flight period for",sp_data_all$SPECIES[1],"at year", y, " weird predicted values"))                    
+                    sp_data_all[, "FITTED"] <- NA
                     sp_data_all[, "COUNT_IMPUTED"] <- NA
                     sp_data_all[is.na(sp_data_all$COUNT), "COUNT_IMPUTED"] <- NA
                     sp_data_all[, "NM"] <- NA    
